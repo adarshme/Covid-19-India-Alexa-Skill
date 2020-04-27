f288217d69fa1a3eb4b5526041129bacaf26c8f3
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2020-04-27 19:29:12 +0530

    Added AWS Lambda Function Code

---
+++ AWS Lambda Function/lambda_function.py
@@ -0,0 +1,52 @@
+import base64
+import json
+import os
+import urllib
+from urllib import request, parse
+
+TWILIO_SMS_URL = "https://api.twilio.com/2010-04-01/Accounts/{}/Messages.json"
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
+
+def lambda_handler(event, context):
+    # TODO implement
+    return sendSMS(event)
+
+def sendSMS(event):
+    to_number = event['queryStringParameters']['To']
+    from_number = event['queryStringParameters']['From']
+    body = event['queryStringParameters']['Body']
+    
+    if not TWILIO_ACCOUNT_SID:
+        return "Unable to access Twilio Account SID."
+    elif not TWILIO_AUTH_TOKEN:
+        return "Unable to access Twilio Auth Token."
+    elif not to_number:
+        return "The function needs a 'To' number in the format +12023351493"
+    elif not from_number:
+        return "The function needs a 'From' number in the format +19732644156"
+    elif not body:
+        return "The function needs a 'Body' message to send."
+    
+    populated_url = TWILIO_SMS_URL.format(TWILIO_ACCOUNT_SID)
+    post_params = {"To": to_number, "From": from_number, "Body": body}
+    
+    data = parse.urlencode(post_params).encode()
+    req = request.Request(populated_url)
+    
+    authentication = "{}:{}".format(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    base64string = base64.b64encode(authentication.encode('utf-8'))
+    req.add_header("Authorization", "Basic %s" % base64string.decode('ascii'))
+    
+    try:
+        # perform HTTP POST request
+        with request.urlopen(req, data) as f:
+            print("Twilio returned {}".format(str(f.read().decode('utf-8'))))
+    except Exception as e:
+        # something went wrong!
+        return e
+    
+    return {
+        'statusCode':200,
+        'body': json.dumps("SMS sent successfully!")
+    }
